69ff338a40b89c43a5199eeded78271415c0fddb
Ishmael Mavundla <v7.vundla@gmail.com>
2025-05-27 15:50:03 +0200

    Restored README.md

---
+++ 1-multi_language.js
@@ -0,0 +1,10 @@
+// Declaring variables
+const lineOne = "C is fun";
+const lineTwo = "Python is cool";
+const lineThree = "JavaScript is amazing";
+
+// Logging the variable into the console
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
+
