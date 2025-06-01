bbdf1ab3f5fb0e3e435f2be2123e8a61a79fcb22
Ishmael Mavundla <v7.vundla@gmail.com>
2025-06-01 23:42:13 +0200

    Add script to print C is fun x times or handle missing argument

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const times = Number(process.argv[2]);
+
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < times) {
+    console.log('C is fun');
+    i++;
+  }
+}
